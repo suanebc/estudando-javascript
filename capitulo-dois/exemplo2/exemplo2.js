b5f824912130a5af0bc1e08c92aed8a611b4e1cb
suanebc <60412898+suanebc@users.noreply.github.com>
2026-03-13 20:10:23 -0300

    novo exemplo

---
+++ capitulo-dois/exemplo2/exemplo2.js
@@ -0,0 +1,14 @@
+const frm = document.querySelector("form");
+const resposta1 = document.querySelector("h3");
+const resposta2 = document.querySelector("h4");
+
+frm.addEventListener("submit", (e) =>{
+  const titulo = frm.inTitulo.value
+  const duracao = Number(frm.inDuracao.value)
+
+  const horas = Math.floor(duracao / 60)
+  const minutos =duracao % 60
+
+  resposta1.innerText = titulo
+  resposta2.innerText = `${horas} horas e ${minutos} minutos`
+})
