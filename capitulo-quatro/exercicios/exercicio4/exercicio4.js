0bbf2b9a8cdd6a71b25aa7316ee46e02ef9cec95
suanebc <60412898+suanebc@users.noreply.github.com>
2026-03-23 20:25:38 -0300

    exercicio 4

--- capitulo-quatro/exercicios/exercicio4/exercicio4.js
+++ capitulo-quatro/exercicios/exercicio4/exercicio4.js
@@ -9,5 +9,22 @@ frm.addEventListener("submit", (e) => {
   const ladoA = Number(frm.inLadoA.value);
   const ladoB = Number(frm.inLadoB.value);
   const ladoC = Number(frm.inLadoC.value);
-  
+
+  if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA) {
+    respostaTriangulo.innerHTML = "Lados podem formar Triangulo";
+
+    if (ladoA === ladoB && ladoA === ladoC) {
+      respostaTipo.innerText = "Tipo : Triângulo Equilátero";
+
+    } else if (ladoA === ladoB || ladoA === ladoC || ladoB === ladoC) {
+      respostaTipo.innerText = "Triângulo Isósceles  - dois lados iguais";
+
+    } else {
+      respostaTipo.innerText = "Triângulo Escaleno - todos os lados diferentes";
+    }
+  } else {
+    respostaTriangulo.innerHTML = "Lados não forma um Triângulo";
+  }
+
+
 })
