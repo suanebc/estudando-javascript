249f79fa3261cd3594f2a7d19556333666df531b
suanebc <60412898+suanebc@users.noreply.github.com>
2026-03-29 22:18:47 -0300

    exemplo clinica odontológica

---
+++ capitulo6/exemplos/exemplo1.js
@@ -0,0 +1,50 @@
+const frm = document.querySelector("form");
+const respostaNome = document.querySelector("span");
+const respostaLista = document.querySelector("pre");
+
+const pacientes = [];
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nome = frm.inPaciente.value;
+  pacientes.push(nome);
+  let lista = "";
+  for (let i = 0; i < pacientes; i++) {
+    lista += `${i + 1}. ${pacientes[i]}\n`;
+  }
+  respostaLista.innerText = lista;
+  frm.inPaciente.value = "";
+  frm.inPaciente.focus();
+});
+
+frm.btUrgencia.addEventListener("ckick", () => {
+  if (!frm.checkValidity()) {
+    alert("Informe o nome do paciente a ser atendido em caráter de urgência");
+    frm.inPaciente.focus();
+    return;
+  }
+  const nome = frm.inPaciente.value;
+  pacientes.unshift(nome);
+  pacientes.forEach((paciente, i) => (lista += `${i + 1}. ${paciente}\n`));
+  respostaLista.innerText = lista;
+  frm.inPaciente.value = "";
+  frm.inPaciente.focus();
+})
+frm.btAtender.addEventListener("click", () => {
+  if (pacientes.length == 0) {
+    alert("Não há pacientes na fila de espera");
+    frm.inPaciente.focus();
+    return;
+  }
+
+  const atender = pacientes.shift();
+  respostaNome.innerText = atender;
+  let lista = "";
+  pacientes.forEach((paciente, i) => (lista += `${i + 1}. ${paciente}\n`));
+  respostaLista.innerText = lista
+
+})
+
+
+
