af1dd426cbea6a1be2ea52c398ba0488c78f05ef
suanebc <60412898+suanebc@users.noreply.github.com>
2026-04-06 19:42:50 -0300

    exemplo3 arquivo js

--- capitulo6/exemplos/exemplo3/exemplo3.js
+++ capitulo6/exemplos/exemplo3/exemplo3.js
@@ -12,7 +12,7 @@ frm.addEventListener("submit", (e) => {
   carros.push({ modelo, preco });
   frm.inModelo.value = "";
   frm.inPreco.value = "";
-  inModelo.focus();
+  frm.inModelo.focus();
 
   frm.btListar.dispatchEvent(new Event("click"));
 
@@ -25,11 +25,42 @@ frm.btListar.addEventListener("Click", () => {
   }
   const lista = carros.reduce((acumulador, carro) =>
     acumulador + carro.modelo + " - R$: " + carros.preco.toFixed(2) + "\n");
-  resposta.innerText = `lista dos carros cadastrados\n${"-".repeat(40)}\n ${lista}`
+  resposta.innerText = `lista dos carros cadastrados\n${"-".repeat(40)}\n ${lista}`;
 
 })
 
 frm.btFiltrar.addEventListener("click", () =>{
-  const maximo = Number(prompt("Qual o valor máximo que o cliente deseja pagar?"))
+  const maximo = Number(prompt("Qual o valor máximo que o cliente deseja pagar?"));
+  if(maximo == 0 || isNaN(maximo)) {
+    return;
+  }
+  const carrosFilter = carros.filter(carro => carro.preco <= maximo)
+  if(carrosFilter.length == 0 ){
+    alert("Não há carros com preço inferiorou igaul ao solicitado");
+    return;
+  }
+  let lista = "";
+  for(const carro of carrosFilter){
+    lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`;
+  }
+  resposta.innerText = `Carros até R$ ${maximo.toFixed(2)}\n${"-".repeat(40)}\n${lista}`;
+});
+
+frm.btSimular.addEventListener("click", () => {
+  const desconto = Number(prompt("Qual o percentual de desconto:"));
+  if(desconto == 0 || isNaN(desconto)) {
+    return;
+  }
+  const carroDesconto = carros.map(auxiliar => ({
+    modelo: auxiliar,
+    preco: auxiliar.preco - (auxiliar.preco * desconto / 100)
+  }));
+  let lista ="";
+  for(const carro of carroDesconto) {
+    lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`;
+  }
+  resposta.innerText = `Carros com desconto: ${desconto} %\n ${"-".repeat(40)}\n${lista}`
 })
+
+
 
