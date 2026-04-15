66389a43c7b7b3afcb786a8cde98acf13ea427ce
suanebc <60412898+suanebc@users.noreply.github.com>
2026-04-15 18:37:24 -0300

    lista de aprovados exercicio 3

--- capitulo6/exercicios/exercicio3/exercicio3.js
+++ capitulo6/exercicios/exercicio3/exercicio3.js
@@ -5,27 +5,28 @@ const resposta = document.querySelector("pre");
 const candidatos = [];
 
 frm.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const nome = frm.inNome.value        // obtém dados do form
-  const acertos = Number(frm.inAcertos.value)
-  candidatos.push({ nome, acertos })
+  e.preventDefault();
+  const nome = frm.inNome.value        
+  const acertos = Number(frm.inAcertos.value);
+  candidatos.push({ nome, acertos });
   frm.reset();
   frm.inNome.focus();
-  frm.btAdicionar.dispatchEvent(new Event("click"));
-}
-
+  frm.btListar.dispatchEvent(new Event("click"));
 });
 
-frm.btAdicionar.addEventListener("click", () => {
 
-  let lista = "";
 
-  for (const candidato of candidatos) {
-    lista += ` ${candidato[0]} - ${candidato[1]}`;
-  }
-  resposta.innerText = lista;
+frm.btListar.addEventListener("click", () => {
+
+  const lista = candidatos.reduce((acumulador,candidatos) =>
+    acumulador +candidatos.nome + " - " +candidatos.acertos + "\n", "")
+  resposta.innerText = `Lista dos candidatos Cadastrados\n${"-".repeat(40)}\n${lista}`
 });
-/*
-f
+
+frm.btAprovados2Fase.addEventListener("click", () => {
+  const acertoS2Fase = prompt("Número de Acertos para aprovação");
+const segundaFase= candidatos.filter((candidato) => {
+  return candidato.acertos >= acertoS2Fase ;
 });
-*/
+resposta.innerText = `Lista dos candidatos Aprovados para 2ª fase\n${"-".repeat(40)}\n${lista}`
+});
