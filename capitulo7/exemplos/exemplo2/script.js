e5d2ba56540a94c28c3a91279b8d4a43023c1581
suanebc <60412898+suanebc@users.noreply.github.com>
2026-04-20 07:06:24 -0300

    segundo exemplo

---
+++ capitulo7/exemplos/exemplo2/script.js
@@ -0,0 +1,40 @@
+const frm = document.querySelector("form"); 
+const resposta = document.querySelector("h3");
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault() ;
+  const senha = frm.inSenha.value;
+  const erros = [];
+
+
+  if (senha.length < 8 || senha.length > 15) {
+    erros.push("entre 8 e 15 caracteres");
+  }
+
+  // verifica se não possui números
+  if (senha.match(/[0-9]/g) == null) {
+    erros.push(" números (no mínimo, 1)");
+  }
+
+ 
+  if (!senha.match(/[a-z]/g)) {
+    erros.push("letras minúsculas (no mínimo, 1)");
+  }
+
+
+  if (!senha.match(/[A-Z]/g) || senha.match(/[A-Z]/g).length == 1) {
+    erros.push("letras maiúsculas (no mínimo 2)");
+  }
+
+
+  if (!senha.match(/[\W|_]/g)) {
+    erros.push("símbolos (no mínimo 1)");
+  }
+
+  // se vetor está vazio (significa que não foram encontrados erros)
+  if (erros.length == 0) {
+    resposta.innerText = "Ok! Senha Válida";
+  } else {
+    resposta.innerText = `Erro... A senha deve possuir ${erros.join(", ")}.`;
+  }
+})
